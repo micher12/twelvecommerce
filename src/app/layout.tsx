6a15e3554b78105e3e5f59dfc8ca79950ce4417a
micher12 <michelasm3@gmail.com>
2025-07-12 19:18:07 -0300

    feat: add paste admin inside (admin) routes paste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
             <body
                 className={`${inter.variable} antialiased dark `}
             >
-   
                 {children}
             </body> 
         </html>
