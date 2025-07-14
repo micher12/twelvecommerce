ad97ca8fe5e0bea27f71b903c2500b9d0813d4e5
micher12 <michelasm3@gmail.com>
2025-07-14 16:52:37 -0300

    update: changed queryClient don't make refetch on 5 min cache.

--- src/lib/useContext.tsx
+++ src/lib/useContext.tsx
@@ -3,7 +3,7 @@
 import { Alert } from "@/components/ui/alert";
 import { Loader } from "@/components/ui/loader";
 import { UseContextProps } from "@/interfaces/use-context-interface";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 const ThisContext = createContext<UseContextProps | null>(null);
@@ -13,7 +13,16 @@ export function ContextProvider({children}: {children: ReactNode}){
     const [alertType, setAlertType] = useState<"sucesso" | "erro" | "warning" | null>(null);
     const [alertMessage, setAlertMessage] = useState<string>("");
     const [loader, setLoader] = useState(false);
-    const queryClient = new QueryClient();
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries:{
+                staleTime: Infinity,   
+                refetchOnMount: false,
+                refetchOnWindowFocus: false,
+                gcTime: 1000 * 5 // 5 min ,
+            }
+        },
+    });
 
     function setAlert(type: typeof alertType, message: string){
         setAlertType(type);
